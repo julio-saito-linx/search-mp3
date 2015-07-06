61263ff6cbe8e26865bb910e8bb2aa1661e1b9f1
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-05 22:52:22 -0300

    search elastic

--- client/components/Index/IndexComponent.jsx
+++ client/components/Index/IndexComponent.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 
 export default class IndexComponent extends React.Component {
   render() {
+
     if (this.props.items.length === 0) {
       return (
         <p ref="empty">Index is empty.</p>
       );
     }
 
+    /**/console.log('\n>>---------\n this.props.items:\n', this.props.items, '\n>>---------\n');/*-debug-*/
+
     return (
       <section>
         <h2>search-mp3</h2>
         <ul ref="indexList" className="index-list">
           {this.props.items.map((item, index) => {
-            return (<li key={index}>item {item}</li>);
+            if (item._source) {
+              return (<li key={index}>{item._source.filename}</li>);
+            }
           })}
         </ul>
       </section>
--- client/entry.jsx
+++ client/entry.jsx
@@ -5,4 +5,23 @@ import 'styles/main.scss';
 import React from 'react/addons';
 import IndexComponent from 'components/Index/IndexComponent.jsx';
 
-React.render(<IndexComponent items={[1, 2, 3]} />, document.body);
+import elasticsearch from 'elasticsearch';
+var client = new elasticsearch.Client({
+  host: 'elasticdatabase.dev.azk.io',
+  log: 'trace'
+});
+
+client.search({
+  index: 'music_library',
+  type: 'song',
+  body: {
+    "query": {
+      "match_all": {}
+    }
+  }
+}).then(function (resp) {
+  var hits = resp.hits.hits;
+  React.render(<IndexComponent items={hits} />, document.body);
+}, function (err) {
+  console.trace(err.message);
+});
